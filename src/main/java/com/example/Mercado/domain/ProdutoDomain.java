ff464bab91d6a3309a4a1af9b22318dfcb6234f9
Kaic-Henrique <107085483+Kaic-Henrique@users.noreply.github.com>
2023-02-08 14:47:21 -0300

    Inicio de um projeto

---
+++ src/main/java/com/example/Mercado/domain/ProdutoDomain.java
@@ -0,0 +1,35 @@
+package com.example.Mercado.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ProdutoDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_profuto;
+
+    private String descricao;
+
+    private Float preco;
+
+    private Float unidade_de_medida_decimal;
+
+    private Integer unidade_de_medida;
+
+    private Integer codigo_interno;
+
+}
